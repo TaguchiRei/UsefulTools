4145427b5369a2a22de69198b3e9fac6f8e6f056
reitaghchi <ren.len2426@gmail.com>
2026-03-16 15:55:29 +0900

    [update]Updateを実行するように

--- Assets/Code/Scripts/GameRule/View/SceneRule.cs
+++ Assets/Code/Scripts/GameRule/View/SceneRule.cs
@@ -1,11 +1,26 @@
+using System;
 using UnityEngine;
 
 public class SceneRule : MonoBehaviour
 {
     [SerializeReference, SubclassSelector] private IRule[] _rules;
-    
+
+    private Action _updates;
+
+    private void Start()
+    {
+        foreach (var rule in _rules)
+        {
+            rule.StartGame();
+            if (rule is IUpdateRule updateRule)
+            {
+                _updates += updateRule.Update;
+            }
+        }
+    }
+
     private void Update()
     {
-        
+        _updates?.Invoke();
     }
 }
