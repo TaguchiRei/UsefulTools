5a52cf3a7362e050e16fef40ddfd5619bdd0a0b0
reitaghchi <ren.len2426@gmail.com>
2026-03-16 17:46:57 +0900

    [add]サウンドインポーター作成開始

---
+++ Assets/Code/Editor/AudioImportWatcher.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioImportWatcher : AssetPostprocessor
+{
+    private void OnPostprocessAudio(AudioClip arg)
+    {
+        
+    }
+}
